a14ef237b66eddc6ba67cd57df6ceab6072bb15f
Shubham Gupta <shubhcs01@gmail.com>
2024-11-28 13:42:53 +0530

    feat: Added actions and controls in component stories

--- src/components/atoms/InputBox/input-box.stories.ts
+++ src/components/atoms/InputBox/input-box.stories.ts
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from '@storybook/angular';
 import { InputBoxComponent } from './input-box.component';
+import { fn } from '@storybook/test';
 
 const meta: Meta<InputBoxComponent> = {
   title: 'Atom/InputBox',
@@ -13,6 +14,9 @@ const meta: Meta<InputBoxComponent> = {
     label: 'Input Box',
     value: '',
     errorMessage: 'This field is required!',
+    onInput(event) {
+      fn()
+    },
   },
   argTypes: {
     type: {
@@ -22,9 +26,10 @@ const meta: Meta<InputBoxComponent> = {
     value: { control: 'text' },
     placeholder: { control: 'text' },
     required: { control: 'boolean' },
+    isInvalid: { control: 'inline-radio', options:[true, false], if:{arg: 'required'} },
     disabled: { control: 'boolean' },
     label: { control: 'text' },
-    errorMessage: { control: 'text' },
+    errorMessage: { control: 'text', if:{arg: 'isInvalid'}},
     valueChange: { action: 'valueChange' }, // Logs value changes in Storybook
   },
 };
@@ -39,9 +44,20 @@ export const WithPlaceholder: Story = {
   args: {
     placeholder: 'Enter your name',
   },
+  // argTypes: {
+  //   // foo is the property we want to remove from the UI
+  //   required: {
+  //     table: {
+  //       disable: true,
+  //     },
+  //   },
+  // },
 };
 
 export const RequiredField: Story = {
+  argTypes:{
+    required: { control: 'boolean' },
+  },
   args: {
     required: true,
     errorMessage: 'This field is required!',
